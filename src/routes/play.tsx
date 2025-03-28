2ca3ca1f44a9bdfe849a9aba632e8a2f62035732
Conor Walton <7566870+waltoncon@users.noreply.github.com>
2025-03-28 15:43:54 +0000

    update play page to use zustand store

--- src/routes/play.tsx
+++ src/routes/play.tsx
@@ -1,42 +1,40 @@
 import { Button } from "@/components/ui/button";
-import winLogic, { Hand } from "@/utils/win-logic";
+import { useGameStore } from "@/utils/game-store";
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
 
 export const Route = createFileRoute("/play")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [player1, setPlayer1] = useState<Hand>();
-  const [player2, setPlayer2] = useState<Hand>();
-  const [gameResult, setGameResult] = useState<
-    "player1" | "player2" | "draw"
-  >();
+  const playerChoose = useGameStore((state) => state.playerChoose);
+  const result = useGameStore((state) => state.result);
 
-  useEffect(() => {
-    if (!player1 || !player2) return;
-    const result = winLogic(player1, player2);
-    setGameResult(result);
-  }, [player1, player2]);
+  const state = useGameStore((state) =>
+    JSON.stringify({
+      player1: state.player1,
+      player2: state.player2,
+    }),
+  );
 
   return (
     <main>
       <div>
-        <Button onClick={() => setPlayer1("rock")}>✊</Button>
-        <Button onClick={() => setPlayer1("paper")}>✋</Button>
-        <Button onClick={() => setPlayer1("scissors")}>✌️</Button>
-        <Button onClick={() => setPlayer1("lizard")}>🤏</Button>
-        <Button onClick={() => setPlayer1("spock")}>🖖</Button>
+        <Button onClick={() => playerChoose("player1", "rock")}>✊</Button>
+        <Button onClick={() => playerChoose("player1", "paper")}>✋</Button>
+        <Button onClick={() => playerChoose("player1", "scissors")}>✌️</Button>
+        <Button onClick={() => playerChoose("player1", "lizard")}>🤏</Button>
+        <Button onClick={() => playerChoose("player1", "spock")}>🖖</Button>
       </div>
       <div>
-        <Button onClick={() => setPlayer2("rock")}>✊</Button>
-        <Button onClick={() => setPlayer2("paper")}>✋</Button>
-        <Button onClick={() => setPlayer2("scissors")}>✌️</Button>
-        <Button onClick={() => setPlayer2("lizard")}>🤏</Button>
-        <Button onClick={() => setPlayer2("spock")}>🖖</Button>
+        <Button onClick={() => playerChoose("player2", "rock")}>✊</Button>
+        <Button onClick={() => playerChoose("player2", "paper")}>✋</Button>
+        <Button onClick={() => playerChoose("player2", "scissors")}>✌️</Button>
+        <Button onClick={() => playerChoose("player2", "lizard")}>🤏</Button>
+        <Button onClick={() => playerChoose("player2", "spock")}>🖖</Button>
       </div>
-      <h1>{gameResult}</h1>
+      <h1>{result}</h1>
+      <pre>{state}</pre>
     </main>
   );
 }
