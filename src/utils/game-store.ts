668ec19f8a17453b7166be5ee104474c167ffb60
Conor Walton <7566870+waltoncon@users.noreply.github.com>
2025-03-28 15:42:33 +0000

    change how game state is stored

--- src/utils/game-store.ts
+++ src/utils/game-store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import winLogic, { Hand } from "./win-logic";
+import { devtools } from "zustand/middleware";
 
 export const computer = Symbol("computer-player");
 
@@ -11,31 +12,38 @@ interface GameState {
   setPlayers: (player1: string, player2?: string) => void;
 }
 
-export const gameStore = create<GameState>()((set, get) => ({
-  player1: undefined,
-  player2: undefined,
-  result: undefined,
-  playerChoose(player, hand) {
-    set((state) => ({
-      ...state,
-      [player]: hand,
-    }));
+export const useGameStore = create<GameState>()(
+  devtools((set, get) => ({
+    player1: { name: "Someone" },
+    player2: { name: "Other" },
+    result: undefined,
+    playerChoose(player, hand) {
+      console.log("playerChoose", player, hand);
 
-    const { player1, player2 } = get();
-
-    if (player1?.hand && player2?.hand) {
-      const result = winLogic(player1.hand, player2.hand);
       set((state) => ({
         ...state,
-        result: result,
+        [player]: {
+          ...state[player],
+          hand: hand as Hand,
+        },
       }));
-    }
-  },
-  setPlayers(player1, player2) {
-    return set((state) => ({
-      ...state,
-      player1: { name: player1 },
-      player2: { name: player2 || computer },
-    }));
-  },
-}));
+
+      const { player1, player2 } = get();
+
+      if (player1?.hand && player2?.hand) {
+        const result = winLogic(player1.hand, player2.hand);
+        set((state) => ({
+          ...state,
+          result: result,
+        }));
+      }
+    },
+    setPlayers(player1, player2) {
+      return set((state) => ({
+        ...state,
+        player1: { name: player1 },
+        player2: { name: player2 || computer },
+      }));
+    },
+  })),
+);
