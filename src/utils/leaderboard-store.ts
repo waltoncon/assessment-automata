e6e6c80ea6238e370fa4e869e0ecddc5d04fa28f
Conor Walton <7566870+waltoncon@users.noreply.github.com>
2025-03-28 17:53:11 +0000

    add clearBoard function

--- src/utils/leaderboard-store.ts
+++ src/utils/leaderboard-store.ts
@@ -1,20 +1,18 @@
+import { toast } from "sonner";
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
 type LeaderboardState = {
   board: Record<string, number>;
   addScore: (username: string, score?: number) => void;
+  clearBoard: () => void;
 };
 
 export const useLeaderboardStore = create<LeaderboardState>()(
   devtools(
     persist(
       (set) => ({
-        board: {
-          SuperCoolGuy: 10,
-          anotherUser: 5,
-          "ME!!": 2,
-        },
+        board: {},
         addScore: (username: string, score = 1) =>
           set((state) => ({
             ...state,
@@ -23,6 +21,12 @@ export const useLeaderboardStore = create<LeaderboardState>()(
               [username]: (state.board[username] || 0) + score,
             },
           })),
+        clearBoard() {
+          set(() => ({
+            board: {},
+          }));
+          toast("Leaderboard cleared");
+        },
       }),
       {
         name: "leaderboard-store",
